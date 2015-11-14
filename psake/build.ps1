7ae2c98dc8db12802756cb14539caa1431a84f9f
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2015-11-14 08:42:22 +0200

    init

---
+++ psake/build.ps1
@@ -0,0 +1,14 @@
+﻿cls
+
+Remove-Module [p]sake
+
+#Import-Module ..\packages\psake.*\tools\psake.psm1
+$psakeModule = ((Get-ChildItem ("..\packages\psake.4.4.2\tools\psake.psm1")).FullName | Sort-Object $_ | select -Last 1)
+
+Import-Module $psakeModule
+
+#Invoke-psake -buildFile .\default.ps1 -taskList Test -properties @{ "testMessage" = "What am I doing? "}
+Invoke-psake -buildFile .\default.ps1 `
+			 -taskList Test `
+			 -properties @{"testMessage"="Testing"}`
+			 -parameters @{"solutionFile" = "..\psake.sln"}
---
+++ psake/default.ps1
@@ -0,0 +1,40 @@
+﻿properties {
+	$testMessage = 'Executed Test!'
+	$compileMessage = 'Executed Compile!'
+	$cleanMessage = 'Executed Clean!'
+
+	$solutionDirectory = (Get-Item $solutionFile).DirectoryName
+	$outputDirectory = "$solutionDirectory\.build"
+	$temporaryOutputDirectory = "$outputDirectory\temp"
+}
+
+task default -depends Test
+
+task Init -description "Initializes the build by removing previous `
+artifacts and creating output directories"`
+-requiredVariables outputDirectory, temporaryOutputDirectory {
+	#Remove previous build results
+	if (Test-Path $outputDirectory)  {
+		Write-Host "Removing output directory located at $outputDirectory"
+		Remove-Item $outputDirectory -Force -Recurse
+	}
+	
+	Write-Host "Creating output directory located at ..\build"
+	New-Item $outputDirectory -ItemType Directory | Out-Null
+
+	Write-Host "Creating temporary directory located at $temporaryOutputDirectory"
+	New-Item $temporaryOutputDirectory -ItemType Directory | Out-Null
+
+}
+
+task Clean -description "Remove temporary files" {
+	Write-Host $cleanMessage
+}
+
+task Compile -depends Init -description "Compile the code" {
+	Write-Host $compileMessage
+}
+
+task Test -depends Compile, Clean -description "Run the unit tests" {
+	Write-Host $testMessage
+}
