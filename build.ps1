788eda43475d4b2af390788582b49bfcbcd1dc70
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2015-11-14 09:27:14 +0200

    build

--- build.ps1
+++ build.ps1
@@ -3,12 +3,12 @@
 Remove-Module [p]sake
 
 #Import-Module ..\packages\psake.*\tools\psake.psm1
-$psakeModule = ((Get-ChildItem ("..\packages\psake.4.4.2\tools\psake.psm1")).FullName | Sort-Object $_ | select -Last 1)
+$psakeModule = ((Get-ChildItem (".\packages\psake*\tools\psake.psm1")).FullName | Sort-Object $_ | select -Last 1)
 
 Import-Module $psakeModule
 
 #Invoke-psake -buildFile .\default.ps1 -taskList Test -properties @{ "testMessage" = "What am I doing? "}
-Invoke-psake -buildFile .\default.ps1 `
+Invoke-psake -buildFile .\psake\default.ps1 `
 			 -taskList Test `
 			 -framework 4.5.2 `
 			 -properties @{ 
